338ebda302bb976e7959e88f08703e4d4aaf62be
Alejandro <24392662+chicoesqueleto@users.noreply.github.com>
2024-05-11 17:02:30 -0500

    nav shrink starts

--- assets/js/nav-shrink.js
+++ assets/js/nav-shrink.js
@@ -1,5 +1,5 @@
 $(window).scroll(function() {
-  if ($(document).scrollTop() > 4) {
+  if ($(document).scrollTop() > 1) {
     $('#main-nav-desk').addClass('nav-shrink uk-animation-slide-top');
   } else {
     $('#main-nav-desk').removeClass('nav-shrink uk-animation-slide-top');
